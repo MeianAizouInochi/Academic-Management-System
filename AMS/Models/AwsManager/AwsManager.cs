2ad512c1bdaf0a55164b9f435a6979bebdd697ba
Meian <meianaizouinochi@gmail.com>
2023-03-29 14:04:58 +0530

    Created the Model

---
+++ AMS/Models/AwsManager/AwsManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.Models.AwsManager
+{
+    public class Aws_Manager
+    {
+
+    }
+}
