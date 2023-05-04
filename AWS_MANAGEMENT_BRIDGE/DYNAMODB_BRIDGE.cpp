d3b9540b1c53438d423113db0ab8becaab304100
jashandeep singh <js400951@gmail.com>
2023-05-04 12:37:54 +0530

    added functionality to pass Map from C++ to .net in order to access data properly.
    made changes in DYNAMODB_BRIDGE for same.

--- AWS_MANAGEMENT_BRIDGE/DYNAMODB_BRIDGE.cpp
+++ AWS_MANAGEMENT_BRIDGE/DYNAMODB_BRIDGE.cpp
@@ -16,24 +16,24 @@ namespace AWS_MANAGEMENT_BRIDGE
 
 	}
 
-	array<String^>^ DYNAMODB_BRIDGE::GetItems(String^ tablename, String^ partitionkey, String^ partitionvalue)
+	Dictionary<String^, String^>^ DYNAMODB_BRIDGE::GetItems(String^ tablename, String^ partitionkey, String^ partitionvalue)
 	{
 		std::string table_name = (const char*)(Marshal::StringToHGlobalAnsi(tablename)).ToPointer();
 		std::string partition_key = (const char*)(Marshal::StringToHGlobalAnsi(partitionkey)).ToPointer();
 		std::string partition_value = (const char*)(Marshal::StringToHGlobalAnsi(partitionvalue)).ToPointer();
 												
-		std::vector<std::string> Temp_vector = M_Instance->GetItems(table_name, partition_key, partition_value);
+		std::map<std::string,std::string>* Temp_map = M_Instance->GetItems(table_name, partition_key, partition_value);
 
-		int size = Temp_vector.size();
+		auto result = gcnew Dictionary<String^, String^>();
 
-		array<String^>^ Array_Of_Objects = gcnew array<String^>(size);
 
-		for (int i = 0; i < size; i++)
+		for (auto it = Temp_map->begin(); it != Temp_map->end(); ++it)
 		{
-			Array_Of_Objects[i] = gcnew String(Temp_vector[i].c_str());
+			result->Add(gcnew String(it->first.c_str()), gcnew String(it->second.c_str()));
 		}
 
-		return Array_Of_Objects;
+
+		return result;
 	}
 
 	array<String^>^ DYNAMODB_BRIDGE::ListTables()
